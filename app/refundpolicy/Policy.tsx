d9e82430a4d77791d13a245d4d8070866eee5cdd
Aditya Tyagi <adityatyagiav1@gmail.com>
2024-03-31 21:07:10 +0530

    refund page updated

--- app/refundpolicy/Policy.tsx
+++ app/refundpolicy/Policy.tsx
@@ -14,7 +14,7 @@ const Policy = (props: Props) => {
   <div className="px-4 max-w-screen-xl pb-20 mx-auto prose">
     <h2 className='text-3xl'>Refund Policy:</h2>
     <p className='mt-3'>
-    At Quality Veda, we strive to ensure your satisfaction with our training programs. If you are not entirely satisfied with your purchase, we{`'`}re here to help.    </p>
+    At Quality Veda, we are committed to providing you with a seamless experience with our training programs. If for any reason you are not entirely satisfied with your purchase, we are here to assist you.</p>
 
     <h2 className='text-3xl mt-8'>1. Eligibility for Refund:</h2>
 
@@ -22,7 +22,7 @@ const Policy = (props: Props) => {
     Refunds may be considered under the following circumstances:
     <ul>
         <li>•  The training program was canceled by Quality Veda.</li>
-        <li>•  You are unable to attend the training program due to unforeseen circumstances, and you notify us within the specified cancellation period (which is 24 hrs before starting the live class).</li>
+        <li>•  You are unable to attend the training program due to unforeseen circumstances, and you notify us within the specified cancellation period, which is 24 hours before the commencement of the live class.</li>
     </ul>
 
     </p>
@@ -32,17 +32,11 @@ const Policy = (props: Props) => {
     
     <h2 className='text-3xl mt-8'>2. Refund Process:</h2>
     <p className='mt-3'>
-    To request a refund, please contact us at +91 888 250 9194 within the specified refund period.
-Your request will be reviewed, and if deemed eligible, we will initiate the refund process.
-Refunds will be issued to the original payment method used for the purchase.
-
-
-
+    To request a refund, please contact us at +91 888 250 9194 within the specified refund period. Your request will be promptly reviewed, and if deemed eligible, we will initiate the refund process. Refunds will be issued to the original payment method used for the purchase and may take 5-7 working days to reflect in your bank account.
     </p>
     <h2 className='text-3xl mt-8'>3. Refund Period:</h2>
     <p className='mt-3'>
-    The refund period may vary depending on the specific training program and terms of purchase. Please refer to the terms and conditions provided at the time of registration for details on the refund period applicable to your purchase.
-
+    The refund period may vary depending on the specific training program and terms of purchase. Please refer to the terms and conditions provided at the time of registration for details on the applicable refund period.
     </p>
 
     <h2 className='text-3xl mt-8'>4. Non-Refundable Items:</h2>
@@ -61,21 +55,21 @@ Refunds will be issued to the original payment method used for the purchase.
 
     <h2 className='text-3xl mt-8'>5. Cancellation Policy:</h2>
     <p className='mt-3'>
-    If you need to cancel your registration for a training program, please notify us as soon as possible.
-Cancellation deadlines and penalties may apply. Please refer to the terms and conditions provided at the time of registration for details on the cancellation policy applicable to your purchase.
+    If you need to cancel your registration for a training program, please notify us as soon as possible. Cancellation deadlines and penalties may apply. Please refer to the terms and conditions provided at the time of registration for details on the cancellation policy applicable to your purchase.
 
     </p>
     
     
     <h2 className='text-3xl mt-8'>6. Contact Us:</h2>
     <p className='mt-3'>
-    If you have any questions or concerns about our refund policy, please contact us at <a className='text-blue-800' href='tel:+918882509194.'>+91 8882509194.</a>. Our team is here to assist you and ensure a positive experience with Quality Veda. </p>
+    If you have any questions or concerns about our refund policy, please contact us at <a className='text-blue-800' href='tel:+918882509194.'>+91 8882509194.</a>. or email us at <a className='text-blue-800' href='mailto:your.qualityveda@gmail.com'>your.qualityveda@gmail.com</a>. Our team is dedicated to assisting you and ensuring a positive experience with Quality Veda.</p>
 
 
     <h2 className='text-3xl mt-8'>7. Changes to Refund Policy:</h2>
     <p className='mt-3'>
-    Quality Veda reserves the right to modify or update this refund policy at any time without prior notice. Any changes will be effective immediately upon posting on our website or other communication channels.
-Thank you for choosing Quality Veda. We appreciate your trust and support in our training programs.
+    Quality Veda reserves the right to modify or update this refund policy at any time without prior notice. Any changes will be effective immediately upon posting on our website or other communication channels. 
+    <br />
+    Thank you for choosing Quality Veda. We appreciate your trust and support in our training programs.
   </p>
 
 
