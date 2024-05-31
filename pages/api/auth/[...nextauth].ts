37cc919b699fd2e942a6388c84874e2ace35b61b
Aditya Tyagi <adityatyagiav1@gmail.com>
2024-05-31 20:39:55 +0530

    again fixed

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,29 +2,52 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 
-
 const NEXTAUTH_URL = process.env.NEXTAUTH_URL || "https://qualityveda.co";
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-      authorization: {
-        params: {
-          redirect_uri: `https://qualityveda.co/api/auth/callback/google`,
-        },
-      },
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
     }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID || '',
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || ''
     }),
   ],
   secret: process.env.SECRET,
   callbacks: {
-    async redirect({ url, baseUrl}:{ url: string, baseUrl: string }) {
+    async redirect({ url, baseUrl }: { url: string, baseUrl: string }) {
       return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+  },
+  // Add this configuration for secure cookies
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true
+      },
+    },
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        sameSite: 'lax',
+        path: '/',
+        secure: true
+      },
+    },
+    csrfToken: {
+      name: `__Host-next-auth.csrf-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true
+      },
     },
   },
 };
