322afde394c3d1b644439222b7bf35ea2467e756
Aditya Tyagi <adityatyagiav1@gmail.com>
2024-05-31 16:52:49 +0530

    next auth error fixed

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,32 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
-console.log(process.env.GOOGLE_CLIENT_ID,'red');
+
+
+const NEXTAUTH_URL = process.env.NEXTAUTH_URL || "";
+
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || '',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-        }),
-        GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID || '',
-            clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
-        })
-    ],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      authorization: {
+        params: {
+          redirect_uri: `${NEXTAUTH_URL}/api/auth/callback/google`,
+        },
+      },
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
+    }),
+  ],
   secret: process.env.SECRET,
-}
+  callbacks: {
+    async redirect({ url, baseUrl}:{ url: string, baseUrl: string }) {
+      return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+  },
+};
 
 export default NextAuth(authOptions);
