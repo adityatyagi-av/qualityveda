222af523547e4e0672147b00fd6290d08871ade4
Ayushisharma30 <104419373+ayu3011@users.noreply.github.com>
2024-08-14 20:22:18 +0530

    New Meta descriptions and Page titles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,8 +8,8 @@ const Page = (props: Props) => {
   return (
     <div className="dark:bg-hsl-custom">
       <Heading
-        title="About Quality Veda | Leading Medical Training Platform"
-        description=": Quality Veda, a known medical education, Training and Consultancy service provider can help you enhance t quality of your labs and hospitals. Get Quality control training and Consultancy."
+        title="About Quality Veda| Best ISO Training and NABL Consultancy"
+        description=":  Quality Veda, a known medical education, Training and Consultancy service provider can help you enhance the quality of your labs and hospitals. Get Quality control training and Consultancy."
         keywords="ISO 15189 training.	4 days iso 15189 training,	Laboratory quality control,	Quality manager training,	quality manager certification online,	ISO 15189 certification,	Quality management systems,	Laboratory accreditation,	Internal quality control,	ISO 15189 version 2022 pdf,	Medical laboratory training,	ISO 15189 consultant,	iso 15189 version 2022 training,	iso 15189 version 2022 certificate,	Quality assurance in healthcare,	Pathology laboratory training,	Clinical laboratory standards, ISO 15189 checklist, Laboratory quality improvement, ISO 15189 courses, Phlebotomy training , Laboratory quality management , NABL Accreditation"
       />
      <AboutHero/>
