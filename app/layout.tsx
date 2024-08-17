36de2a60b1f15125b49cb83d15d0872586fc270f
Aditya Tyagi <adityatyagiav1@gmail.com>
2024-08-17 23:18:27 +0530

    gta code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,15 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PXZLWFQX');
+        `}
+    </Script>
       <body
         className={`${poppins.variable} ${josefin.variable} !bg-white dark:bg-hsl-custom bg-no-repeat dark:bg-gradient-to-b dark:from-hsl-custom dark:to-hsl-custom duration-300`}
       >
@@ -91,6 +100,11 @@ export default function RootLayout({
           />
         </noscript>
         {/* <!-- End Meta Pixel Code --> */}
+        <noscript
+        dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PXZLWFQX" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+        }} 
+    />
       </body>
     </html>
   );
