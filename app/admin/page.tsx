99df875497cccf85a9691ff147e9ebc6d39d6cc1
ADITYA TYAGI <110872747+adityatyagi-av@users.noreply.github.com>
2024-08-17 23:36:09 +0530

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -16,7 +16,6 @@ const page = (props: Props) => {
         description=" ISO 15189 training for medical laboratories! QualityVeda's expert-led program helps achieve accreditation, improve quality control, and deliver exceptional patient care."
         keywords="ISO 15189 training.	4 days iso 15189 training,	Laboratory quality control,	Quality manager training,	quality manager certification online,	ISO 15189 certification,	Quality management systems,	Laboratory accreditation,	Internal quality control,	ISO 15189 version 2022 pdf,	Medical laboratory training,	ISO 15189 consultant,	iso 15189 version 2022 training,	iso 15189 version 2022 certificate,	Quality assurance in healthcare,	Pathology laboratory training,	Clinical laboratory standards, ISO 15189 checklist, Laboratory quality improvement, ISO 15189 courses, Phlebotomy training , Laboratory quality management , NABL Accreditation"
       />
-      />
         <div className="flex min-h-screen">
           <div className="1500px:w-[16%] w-1/5">
             <AdminSidebar />
