39f89480e71513e0be400873e881ea1c1f7c30ad
Ayushisharma30 <104419373+ayu3011@users.noreply.github.com>
2024-08-29 16:32:36 +0530

    Update page.tsx

--- app/admin/hero/page.tsx
+++ app/admin/hero/page.tsx
@@ -13,8 +13,8 @@ const page = (props: Props) => {
     <div>
       <AdminProtected>
       <Heading
-        title="QualityVeda | Elevate Your Laboratory Quality with ISO 15189 Training | About Quality Veda | Leading Medical Training Platform"
-        description=": Quality Veda is your premier destination for diverse medical training programs, bridging theoretical knowledge with practical application. Discover our mission to elevate healthcare standards and our vision for excellence in patient care."
+        title="QualityVeda | Best ISO 15189 training and NABL Consultancy in New Delhi"
+        description=":" ISO 15189 training for medical laboratories! QualityVeda's expert-led program helps achieve accreditation, improve quality control, and deliver exceptional patient care.
         keywords="ISO 15189 training.	4 days iso 15189 training,	Laboratory quality control,	Quality manager training,	quality manager certification online,	ISO 15189 certification,	Quality management systems,	Laboratory accreditation,	Internal quality control,	ISO 15189 version 2022 pdf,	Medical laboratory training,	ISO 15189 consultant,	iso 15189 version 2022 training,	iso 15189 version 2022 certificate,	Quality assurance in healthcare,	Pathology laboratory training,	Clinical laboratory standards, ISO 15189 checklist, Laboratory quality improvement, ISO 15189 courses, Phlebotomy training , Laboratory quality management , NABL Accreditation"
       />
         <div className="flex h-screen">
